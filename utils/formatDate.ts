326c7b0c69ee48dbc80879bf7f3bcf56b4256799
Vetamusiclab <vetagulliver@gmail.com>
2025-08-25 23:14:44 +0300

    Delete utils directory

--- utils/formatDate.ts
+++
@@ -1,7 +0,0 @@
-export function formatDateRange(startISO: string, endISO: string){
-  const s = new Date(startISO); const e = new Date(endISO);
-  const pad = (n:number)=> String(n).padStart(2, '0');
-  const date = `${pad(s.getDate())}.${pad(s.getMonth()+1)}.${s.getFullYear()}`;
-  const time = `${pad(s.getHours())}:${pad(s.getMinutes())}-${pad(e.getHours())}:${pad(e.getMinutes())}`;
-  return `${date} • ${time}`;
-}

f7fe8ff5c2660f58bf177ea381faecca033570ce
Vetamusiclab <vetagulliver@gmail.com>
2025-08-25 12:58:11 +0300

    Delete components directory

--- components/shared/UserAvatar.tsx
+++
@@ -1,4 +0,0 @@
-import Image from 'next/image'
-export default function UserAvatar({ src='/avatars/default.png', size=40 }:{src?:string; size?:number}){
-  return <Image src={src} alt="avatar" width={size} height={size} className="rounded-full border" />
-}

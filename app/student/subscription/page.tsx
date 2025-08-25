a3ab4f064f7f36d71ead74383f398d05cf2d7782
Vetamusiclab <vetagulliver@gmail.com>
2025-08-25 12:58:22 +0300

    Delete app directory

--- app/student/subscription/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Card, CardContent } from '@/components/ui/Card'
-export default function Subscription(){
-  return (
-    <Card><CardContent>
-      <div className="font-semibold mb-2">Абонемент</div>
-      <div className="text-sm">Тариф: <b>Light</b> • Осталось занятий: <b>3</b> • Оплачен до: <b>30.09.2025</b></div>
-    </CardContent></Card>
-  )
-}

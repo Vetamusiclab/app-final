295727f3d24a2248c18ef3ea3f39d66ed3c399e4
Vetamusiclab <vetagulliver@gmail.com>
2025-08-25 23:28:30 +0300

    Update user.ts

--- types/user.ts
+++ types/user.ts
@@ -1,11 +1,15 @@
 // types/user.ts
 
+// Уровни наставников
 export type TutorLevel = 'Basic' | 'Progressive' | 'Prive';
 
+// Тип пользователя
 export type User = {
   id: string;
   name: string;
   role: 'student' | 'teacher' | 'admin';
   avatar?: string;
   tutorLevel?: TutorLevel;
+  email?: string; // добавлено поле email
+  directions?: string[]; // для учеников с направлениями
 };

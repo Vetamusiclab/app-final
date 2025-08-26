ac4ac3d8a0523097a50fb1dab7e91806a6d64f3e
Vetamusiclab <vetagulliver@gmail.com>
2025-08-26 23:44:48 +0300

    Update page.tsx

--- app/student/subscription/page.tsx
+++ app/student/subscription/page.tsx
@@ -12,17 +12,3 @@ const SubscriptionPage = () => {
 };
 
 export default SubscriptionPage;
-'use client';
-
-import React from 'react';
-
-const SubscriptionPage = () => {
-  return (
-    <div style={{ padding: '20px' }}>
-      <h1>Подписка ученика</h1>
-      <p>Здесь будет отображаться информация о подписке.</p>
-    </div>
-  );
-};
-
-export default SubscriptionPage;

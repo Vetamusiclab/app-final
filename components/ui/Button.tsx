630c2b8623049a316327dfcae6a480ce32efb9a4
Vetamusiclab <vetagulliver@gmail.com>
2025-08-26 22:25:20 +0300

    Update Button.tsx

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,7 +1,8 @@
+// components/ui/Button.tsx
 import { ButtonHTMLAttributes } from "react";
 import clsx from "clsx";
 
-type Props = ButtonHTMLAttributes<HTMLButtonElement> & { variant?: "primary"|"ghost" };
-export default function Button({ className, variant="primary", ...props}: Props){
-  return <button {...props} className={clsx("btn", variant==="primary"?"btn-primary":"btn-ghost", className)} />;
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & { variant?: "primary" | "ghost" };
+export default function Button({ className, variant = "primary", ...props }: Props) {
+  return <button {...props} className={clsx("btn", variant === "primary" ? "btn-primary" : "btn-ghost", className)} />;
 }

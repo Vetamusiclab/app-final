036fba48da20317c72118d71ca3168c337ce6103
Vetamusiclab <vetagulliver@gmail.com>
2025-08-26 22:26:07 +0300

    Update Sidebar.tsx

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,7 +1,8 @@
+// components/layout/Sidebar.tsx
 import Link from "next/link";
 import clsx from "clsx";
 
-export default function Sidebar({ items, title }: { title: string; items: { href: string; label: string; }[] }){
+export default function Sidebar({ items, title }: { title: string; items: { href: string; label: string; }[] }) {
   return (
     <aside className="w-64 shrink-0 p-4 border-r border-black/10">
       <div className="mb-4 font-semibold">{title}</div>

99511ff82d2b2eb40a3e23d9e7ef9f590f963da1
Vetamusiclab <vetagulliver@gmail.com>
2025-08-27 23:18:02 +0300

    Delete components directory

--- components/ui/Button.tsx
+++
@@ -1,8 +0,0 @@
-// components/ui/Button.tsx
-import { ButtonHTMLAttributes } from "react";
-import clsx from "clsx";
-
-type Props = ButtonHTMLAttributes<HTMLButtonElement> & { variant?: "primary" | "ghost" };
-export default function Button({ className, variant = "primary", ...props }: Props) {
-  return <button {...props} className={clsx("btn", variant === "primary" ? "btn-primary" : "btn-ghost", className)} />;
-}
--- components/ui/Input.tsx
+++
@@ -1,7 +0,0 @@
-// components/ui/Input.tsx
-import { InputHTMLAttributes } from "react";
-import clsx from "clsx";
-
-export default function Input(props: InputHTMLAttributes<HTMLInputElement>) {
-  return <input {...props} className={clsx("input", props.className)} />;
-}

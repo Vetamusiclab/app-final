7503378897cacac46ada2e0fca42035b45639b43
Vetamusiclab <vetagulliver@gmail.com>
2025-08-31 16:32:34 +0300

    Create page.tsx

---
+++ app/student/page.tsx
@@ -0,0 +1,7 @@
+export default function StudentPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-white">
+      <h1 className="text-3xl font-bold text-primary">Страница ученика</h1>
+    </main>
+  );
+}

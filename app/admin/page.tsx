0f55cf921c7a183319ae91d6ed5b3384dfc818bb
Vetamusiclab <vetagulliver@gmail.com>
2025-08-31 16:33:44 +0300

    Create page.tsx

---
+++ app/admin/page.tsx
@@ -0,0 +1,7 @@
+export default function AdminPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-white">
+      <h1 className="text-3xl font-bold text-primary">Страница админа</h1>
+    </main>
+  );
+}

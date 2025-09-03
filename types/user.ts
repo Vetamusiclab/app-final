371418fa43fcc415e6ac50c3683252b8010ea7dc
Vetamusiclab <vetagulliver@gmail.com>
2025-09-03 23:26:12 +0300

    Update user.ts

--- types/user.ts
+++ types/user.ts
@@ -5,8 +5,9 @@ export type User = {
   id: string;
   role: Role;
   name: string;
-  avatar?: string;
-  directions?: string[];
+  avatar?: string; // e.g. '/avatars/default.png'
+  directions?: string[]; // optional directions
   phone?: string;
   createdAt?: string;
+  color?: string; // HEX color assigned to teacher (e.g. '#6BCB77')
 };

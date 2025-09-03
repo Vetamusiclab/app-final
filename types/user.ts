33e27982add1cb2507d38050794c585dde11b42f
Vetamusiclab <vetagulliver@gmail.com>
2025-09-03 10:56:03 +0300

    Update user.ts

--- types/user.ts
+++ types/user.ts
@@ -5,8 +5,8 @@ export type User = {
   id: string;
   role: Role;
   name: string;
-  avatar?: string; // относительный путь в /public, например '/avatars/default.png'
-  directions?: string[]; // направления (фортепиано, вокал...)
+  avatar?: string;
+  directions?: string[];
   phone?: string;
   createdAt?: string;
 };

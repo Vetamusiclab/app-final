cc9f4a743e5c6855068b8b289ae68967e13c861c
Vetamusiclab <vetagulliver@gmail.com>
2025-09-03 23:28:54 +0300

    Update Select.tsx

--- components/ui/Select.tsx
+++ components/ui/Select.tsx
@@ -5,27 +5,25 @@ import React from 'react';
 
 type Option = { value: string; label: string };
 
-type Props = {
+type Props = React.SelectHTMLAttributes<HTMLSelectElement> & {
   options: Option[];
-  value: string;
-  onChange: (v: string) => void;
-  className?: string;
-  id?: string;
+  label?: string;
 };
 
-export default function Select({ options, value, onChange, className = '', id }: Props) {
+export default function Select({ options, label, ...rest }: Props) {
   return (
-    <select
-      id={id}
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      className={`px-3 py-2 border rounded ${className}`}
-    >
-      {options.map((o) => (
-        <option key={o.value} value={o.value}>
-          {o.label}
-        </option>
-      ))}
-    </select>
+    <div className="flex flex-col">
+      {label && <label className="text-sm mb-1">{label}</label>}
+      <select
+        {...rest}
+        className="px-3 py-2 border rounded bg-white text-sm"
+      >
+        {options.map((o) => (
+          <option key={o.value} value={o.value}>
+            {o.label}
+          </option>
+        ))}
+      </select>
+    </div>
   );
 }
